1b2970fc30768198b700402101a1c928e1a6864d
TusharChanolian <tusharchanolian@gmail.com>
2021-09-19 14:35:35 +0530

    Final Build

--- movies/models.py
+++ movies/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Movies(models.Model):
-    name = models.CharField("Movie", max_length=255)
+    name = models.CharField("Movie", max_length=255, blank=True, null=True)
     description= models.CharField("Movie desc", max_length=255, blank=True, null=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
     
--- movies/urls.py
+++ movies/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import MovieListView, MovieDetailView, MovieNewView, MovieUpdate, MovieDelete
-
-
+from .models import Movies
 urlpatterns = [
     path('movies',MovieListView.as_view()),
     path('movie/<int:pk>',MovieDetailView.as_view()),
-    path('movie/new',MovieNewView.as_view()),
+    path('movie/new',MovieNewView.as_view(),name='New Movie'),
     path('movie/edit/<int:pk>',MovieUpdate.as_view()),
     path('movie/delete/<int:pk>',MovieDelete.as_view()),
 ]
